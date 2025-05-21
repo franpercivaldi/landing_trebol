e348c89557f14fc1d5d0af958ec20a2f8c1109f2
Octavio Percivaldi <octaviopercivaldi2@gmail.com>
2025-05-21 18:48:23 -0300

    Improved reviews style

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -5,36 +5,34 @@ export const Reviews = (props) => {
         <div className="section-title text-center">
           <h2>Lo que dicen nuestros clientes</h2>
         </div>
-        <div className="row">
+        <div className="testimonials-list">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  <div
-                    className="testimonial"
-                    style={{
-                      minHeight: "230px",
-                      display: "flex",
-                      flexDirection: "row",
-                      alignItems: "flex-start",
-                      gap: "15px",
-                    }}
-                  >
-                    <div className="testimonial-image">
-                      <img
-                        src={d.img}
-                        alt={d.name}
-                        style={{
-                          width: "60px",
-                          height: "60px",
-                          borderRadius: "50%",
-                          objectFit: "cover",
-                        }}
-                      />
-                    </div>
-                    <div className="testimonial-content">
-                      <p style={{ marginBottom: "8px" }}>"{d.text}"</p>
-                      <div className="testimonial-meta">- {d.name}</div>
-                    </div>
+                <div
+                  className="testimonial"
+                  style={{
+                    minHeight: "150px",
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "flex-start",
+                    gap: "15px",
+                  }}
+                >
+                  <div className="testimonial-image">
+                    <img
+                      src={d.img}
+                      alt={d.name}
+                      style={{
+                        width: "60px",
+                        height: "60px",
+                        borderRadius: "50%",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
+                  <div className="testimonial-content">
+                    <p style={{ marginBottom: "8px" }}>"{d.text}"</p>
+                    <div className="testimonial-meta">- {d.name}</div>
                   </div>
                 </div>
               ))
