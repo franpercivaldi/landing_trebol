f820dd844afc4eb87c0ef0985adf9bf20fe862df
Octavio Percivaldi <octaviopercivaldi2@gmail.com>
2025-05-22 20:13:30 -0300

    Fixed nav header on mobile

--- src/components/NavigationHeader.jsx
+++ src/components/NavigationHeader.jsx
@@ -1,48 +1,74 @@
-import { Menu } from "lucide-react";
+import { useState } from "react";
 
 export const NavigationHeader = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // Close menu on navigation (mobile UX)
+  const handleNavClick = () => setMenuOpen(false);
+
   return (
     <nav id="menu" className="navbar navbar-default navbar-fixed-top">
       <div className="header-container">
         <button
           type="button"
-          className="navbar-toggle collapsed"
-          data-toggle="collapse"
-          data-target="#bs-example-navbar-collapse-1"
+          className="navbar-toggle"
+          aria-label="Toggle navigation"
+          aria-expanded={menuOpen}
+          onClick={() => setMenuOpen((open) => !open)}
         >
-          <span className="sr-only">Toggle navigation</span> <span className="icon-bar"></span> <span className="icon-bar"></span>
-          <Menu className="icon-bar" />
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
         </button>
         <a href="#page-top">
           <img src="/img/logo.svg" alt="Logo" className="logo" />
         </a>
-        <ul className="nav navbar-nav navbar-right">
+        <ul className={`nav navbar-nav navbar-right${menuOpen ? " show" : ""}`}>
           <li>
-            <a href="#benefits">¿Por qué elegirnos?</a>
+            <a href="#benefits" onClick={handleNavClick}>
+              ¿Por qué elegirnos?
+            </a>
           </li>
           <li>
-            <a href="#about-us">Nosotros</a>
+            <a href="#about-us" onClick={handleNavClick}>
+              Nosotros
+            </a>
           </li>
           <li>
-            <a href="#our-services">Servicios</a>
+            <a href="#our-services" onClick={handleNavClick}>
+              Servicios
+            </a>
           </li>
           <li>
-            <a href="#allies">Aliados</a>
+            <a href="#allies" onClick={handleNavClick}>
+              Aliados
+            </a>
           </li>
           <li>
-            <a href="#reviews">Testimonios</a>
+            <a href="#reviews" onClick={handleNavClick}>
+              Testimonios
+            </a>
           </li>
           <li>
-            <a href="#team">Equipo</a>
+            <a href="#team" onClick={handleNavClick}>
+              Equipo
+            </a>
           </li>
           <li>
-            <a href="#visit-us">Visitanos</a>
+            <a href="#visit-us" onClick={handleNavClick}>
+              Visitanos
+            </a>
           </li>
           <li>
-            <a href="#faq">FAQ</a>
+            <a href="#faq" onClick={handleNavClick}>
+              FAQ
+            </a>
           </li>
           <li>
-            <a href="#contact">Contactanos</a>
+            <a href="#contact" onClick={handleNavClick}>
+              Contactanos
+            </a>
           </li>
         </ul>
       </div>
