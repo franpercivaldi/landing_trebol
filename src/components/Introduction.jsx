a00b3a73883859f2a2e606537842a37ccc7ff389
Octavio Percivaldi <octaviopercivaldi2@gmail.com>
2025-05-22 19:41:05 -0300

    Deleted unnecesary div tags

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -4,14 +4,12 @@ export const Introduction = (props) => {
       <div className="intro">
         <div className="overlay">
           <div className="container">
-            <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1 className="main-title">{props.data.title}</h1>
-                <p>{props.data.paragraph}</p>
-                <a href="#benefits" className="btn btn-custom">
-                  Saber más
-                </a>{" "}
-              </div>
+            <div className="intro-text">
+              <h1 className="main-title">{props.data.title}</h1>
+              <p>{props.data.paragraph}</p>
+              <a href="#benefits" className="btn btn-custom">
+                Saber más
+              </a>{" "}
             </div>
           </div>
         </div>
