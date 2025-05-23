0912923429748b76e9fb37542338287023755913
Francisco <franpercivaldi@gmail.com>
2025-05-23 23:14:48 +0000

    Add mail service

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,6 +1,8 @@
+// src/components/Services.jsx
 import React, { useState } from 'react';
 import { ServicesImage } from './ServicesImage';
 import QuoteForm from './QuoteForm';
+import emailjs from '@emailjs/browser';
 
 export const Services = ({ data }) => {
   const [modalOpen, setModalOpen] = useState(false);
@@ -16,17 +18,30 @@ export const Services = ({ data }) => {
 
   const openForm = title => {
     const type = mapTitleToServiceType(title);
-    if (!type) {
-      // TODO: mostrar mensaje? Martin seguramente lo quiera que no haga nada
-      return;
-    }
+    if (!type) return;
     setServiceType(type);
     setModalOpen(true);
   };
 
   const handleSubmit = formData => {
-    console.log('Datos enviados:', formData);
-    // TODO: ver que usar para mandar los mails
+    const serviceID  = 'service_1gmj93f';    // tu Service ID
+    const templateID = 'template_shrjv2r';   // tu Template ID
+    const publicKey  = 's1LOgESb2pc8GBKYr';  // tu Public Key
+
+    const templateParams = {
+      serviceType,
+      ...formData,
+    };
+
+    emailjs
+      .send(serviceID, templateID, templateParams, publicKey)
+      .then(() => {
+        alert('Tu solicitud de cotización ha sido enviada correctamente.');
+      })
+      .catch(err => {
+        console.error('Error al enviar cotización:', err);
+        alert('Hubo un error al enviar tu solicitud. ¡Intenta de nuevo más tarde!');
+      });
   };
 
   return (
@@ -45,7 +60,10 @@ export const Services = ({ data }) => {
                   className="col-sm-6 col-md-4 col-lg-4 cursor-pointer"
                   onClick={() => openForm(d.title)}
                 >
-                  <ServicesImage title={d.title} smallImage={d.smallImage} />
+                  <ServicesImage
+                    title={d.title}
+                    smallImage={d.smallImage}
+                  />
                 </div>
               ))}
             </div>
